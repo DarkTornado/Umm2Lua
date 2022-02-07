9d27a9bfaddcd5f78fd19ae8c7a40af89a7085ee
Dark Tornado <dt3141592@naver.com>
2022-02-08 01:55:09 +0900

    불필요하게 생성된 label들 삭제

--- com/darktornado/umm2lua/Compiler.java
+++ com/darktornado/umm2lua/Compiler.java
@@ -13,10 +13,15 @@ public class Compiler {
 		Parser parser = new Parser();
 		StringBuilder result = new StringBuilder();
 		for(int n = 1; n < src.length - 1; n++) {
-			src[n] = "::label" + n + "::\n" + parser.parse(src[n]);
-			result.append(src[n]).append("\n");
+			result.append("::label_" + (n + 1) + "::\n" + parser.parse(src[n]) + "\n");
 		}
-		return result.toString().trim();
+		String _result = result.toString();
+		for(int n = 2; n < src.length; n++) {
+			if (!_result.contains("goto label_" + n)) {
+				_result = _result.replace("::label_" + n + "::\n", "");
+			}
+		}
+		return _result.trim();
 	}
 
 }
