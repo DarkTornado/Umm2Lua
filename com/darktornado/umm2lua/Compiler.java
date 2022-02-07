9585aefdc2e0efca106261356e9730de016025cc
Dark Tornado <dt3141592@naver.com>
2022-02-08 02:23:10 +0900

    변수 이름 변경하는거 구현
    luaj에서 쓸건데, 한글 변수 지원 안함;;

--- com/darktornado/umm2lua/Compiler.java
+++ com/darktornado/umm2lua/Compiler.java
@@ -12,16 +12,47 @@ public class Compiler {
 		String[] src = source.split("\n");
 		Parser parser = new Parser();
 		StringBuilder result = new StringBuilder();
-		for(int n = 1; n < src.length - 1; n++) {
+		
+		/* 모든 줄 사이에 label 넣기 */
+		for (int n = 1; n < src.length - 1; n++) {
 			result.append("::label_" + (n + 1) + "::\n" + parser.parse(src[n]) + "\n");
 		}
+		
 		String _result = result.toString();
+
+		/* 없어도 되는 label들 빼기 */
 		for(int n = 2; n < src.length; n++) {
 			if (!_result.contains("goto label_" + n)) {
 				_result = _result.replace("::label_" + n + "::\n", "");
 			}
 		}
-		return _result.trim();
+		return changeVariableNames(_result.trim());
+	}
+	
+	private String changeVariableNames(String src) {
+		int max = getMaxUmm(src);
+		if (max == 0) return src;
+		if (max == 1) return src.replace("어", "var");
+		StringBuilder _umm = new StringBuilder();
+		for (int n = 0; n < max; n++) {
+			_umm.append("어");
+		}
+		String umm = _umm.toString();
+		int count = max;
+		while (count > 0) {
+			src = src.replace(umm, "var" + count);
+			umm = umm.substring(1);
+			count--;
+		}
+		return src;		
+	}
+	
+	private int getMaxUmm(String src) {
+		StringBuilder umm = new StringBuilder("어");
+		for (int n = 0; ; n++) {
+			if (!src.contains(umm.toString())) return n;
+			umm.append("어");
+		}
 	}
 
 }
