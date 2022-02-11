511dbd0e6af25392a4b35bc545f57091ca0fb0a5
Dark Tornado <dt3141592@naver.com>
2022-02-11 16:39:16 +0900

    Code is beautified

--- src/com/darktornado/umm2lua/Compiler.java
+++ src/com/darktornado/umm2lua/Compiler.java
@@ -1,34 +1,34 @@
 package com.darktornado.umm2lua;
 
 public class Compiler {
-	
+
 	private String source;
-	
+
 	public void setSource(String source) {
 		this.source = source.trim();
 	}
-	
+
 	public String execute() {
 		String[] src = source.split("\n");
 		Parser parser = new Parser();
 		StringBuilder result = new StringBuilder();
-		
+
 		/* 모든 줄 사이에 label 넣기 */
 		for (int n = 1; n < src.length - 1; n++) {
 			result.append("::label_" + (n + 1) + "::\n" + parser.parse(src[n]) + "\n");
 		}
-		
+
 		String _result = result.toString();
 
 		/* 없어도 되는 label들 빼기 */
-		for(int n = 2; n < src.length; n++) {
+		for (int n = 2; n < src.length; n++) {
 			if (!_result.contains("goto label_" + n)) {
 				_result = _result.replace("::label_" + n + "::\n", "");
 			}
 		}
 		return changeVariableNames(_result.trim());
 	}
-	
+
 	private String changeVariableNames(String src) {
 		int max = getMaxUmm(src);
 		if (max == 0) return src;
@@ -44,9 +44,9 @@ public class Compiler {
 			umm = umm.substring(1);
 			count--;
 		}
-		return src;		
+		return src;
 	}
-	
+
 	private int getMaxUmm(String src) {
 		StringBuilder umm = new StringBuilder("어");
 		for (int n = 0; ; n++) {
--- src/com/darktornado/umm2lua/Umm2Lua.java
+++ src/com/darktornado/umm2lua/Umm2Lua.java
@@ -1,31 +1,30 @@
 package com.darktornado.umm2lua;
 
 public class Umm2Lua {
-	
+
 	public static String compile(String source) {
 		Compiler cmp = new Compiler();
 		cmp.setSource(source);
 		return cmp.execute();
 	}
-	
+
 	public static void main(String[] args) {
-		String src = "어떻게\n\n"+
-	            "엄.....\n"+
-	            "엄어,\n"+
-	            "식어!\n"+
-	            "동탄어?준........\n"+
-	            "준....\n"+
-	            "어엄.......... ..........,,,\n"+
-	            "식어어ㅋ\n"+
-	            "어엄어어.\n"+
-	            "식어어ㅋ\n"+
-	            "\n이 사람이름이냐ㅋㅋ";
+		String src = "어떻게\n\n" +
+				"엄.....\n" +
+				"엄어,\n" +
+				"식어!\n" +
+				"동탄어?준........\n" +
+				"준....\n" +
+				"어엄.......... ..........,,,\n" +
+				"식어어ㅋ\n" +
+				"어엄어어.\n" +
+				"식어어ㅋ\n" +
+				"\n이 사람이름이냐ㅋㅋ";
 		System.out.println("----- Before -----");
 		System.out.println(src);
 		System.out.println("----- After -----");
 		System.out.println(compile(src));
 		System.out.println("----- End -----");
 	}
-	
 
 }
