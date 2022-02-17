676b53eb48bd933a06f40a978a7139010dd3b49c
EunDaeng <verp@daum.net>
2022-02-17 13:43:15 +0900

    Fix multiplying more than 2 times

--- src/com/darktornado/umm2lua/Parser.java
+++ src/com/darktornado/umm2lua/Parser.java
@@ -37,12 +37,14 @@ public class Parser {
     }
     
     private String intmultiply (String str) {
-    	  Pattern pattern = Pattern.compile("(.*)([0-9])\\*([0-9])");
-    	  Matcher matcher = pattern.matcher(str);
-    	  if(matcher.find()){
-    	      return matcher.group(1).trim()+Integer.parseInt(matcher.group(2).trim())*Integer.parseInt(matcher.group(3).trim());
-    	  }
-    	  return str;
+    	String result = str;
+    	Pattern pattern = Pattern.compile("(.*)([0-9])\\*([0-9])");
+    	Matcher matcher = pattern.matcher(result);
+    	while(matcher.find()){    // 정규식과 매칭되는 값이 있으면
+    	    result = matcher.group(1).trim()+Integer.parseInt(matcher.group(2).trim())*Integer.parseInt(matcher.group(3).trim());        // 특정 단어 사이의 값을 추출한다
+    	    matcher = pattern.matcher(result);
+    	}
+    	return result;
     }
     
     private String dot2int(String src) {
